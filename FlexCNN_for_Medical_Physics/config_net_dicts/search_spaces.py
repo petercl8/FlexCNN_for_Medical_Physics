8213b4f44dd9ffa45ad649150582c61ba0226efd
petercl8 <lindstrom.peter@gmail.com>
2025-12-04 15:32:38 -0600

    Quick automatic update

--- FlexCNN_for_Medical_Physics/config_net_dicts/search_spaces.py
+++ FlexCNN_for_Medical_Physics/config_net_dicts/search_spaces.py
@@ -36,7 +36,7 @@ config_RAY_SI_learnScale = { # Dictionary for Generator: Sinogram-->Image with n
     ## Data Loading ##
     'SI_normalize': False,
     'SI_learnedScale_init': tune.loguniform(1e-6, 1e3),         # Initial guess for learned multiplier. Only used if SI_normalize=False. While SI_learnedScale_init is a hyperparamter, the actual scale is a learned parameter                                                                
-    'SI_layer_norm': tune.choice(['group', 'none']),
+    'SI_layer_norm': tune.choice(['none']),            # Could also add "group" normalization if you make it go into num_channels evenly.
     'SI_gen_final_activ': tune.choice([nn.ReLU(), None]),
 }
 config_RAY_SI_fixedScale = { # Dictionary for Generator: Sinogram-->Image with normalization and fixed scaling
