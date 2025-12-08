c05c79d21ceebf37f1b08a8c5a7492a805c93c0c
petercl8 <lindstrom.peter@gmail.com>
2025-12-08 17:59:55 -0600

    Quick automatic update

--- FlexCNN_for_Medical_Physics/config_net_dicts/tuned-supervisory-SI.py
+++ FlexCNN_for_Medical_Physics/config_net_dicts/tuned-supervisory-SI.py
@@ -41,7 +41,7 @@ config_SUP_SI = {
   "sino_size":180,
   "SI_normalize": True,
   "SI_fixedScale": 8100,
-  "SI_skip_mode": "none",
+  "SI_skip_mode": "concat", # 'none', 'add', 'concat'
   }
 
 '''
