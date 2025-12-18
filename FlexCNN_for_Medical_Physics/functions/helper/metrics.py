f76525330d688eb7ebaafdfd2b560c86f9acee23
petercl8 <lindstrom.peter@gmail.com>
2025-12-17 21:53:58 -0600

    Quick automatic update

--- FlexCNN_for_Medical_Physics/functions/helper/metrics.py
+++ FlexCNN_for_Medical_Physics/functions/helper/metrics.py
@@ -77,7 +77,7 @@ import torch
 def patchwise_moment_metric(batch_pred,
                             batch_target,
                             moments=[1,2,3],
-                            moment_weights={1:0.8, 2:1.0, 3:0.001},   # dict, e.g., {2:1.0, 3:0.001}
+                            moment_weights={1:30, 2:1.0, 3:0.001},   # dict, e.g., {2:1.0, 3:0.001}
                             patch_size=16,
                             stride=8,
                             eps=1e-6,
