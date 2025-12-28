cfffbc25a066ba27f394f65018fa1286ca2c7f01
petercl8 <lindstrom.peter@gmail.com>
2025-12-28 17:27:37 -0600

    Quick automatic update

--- FlexCNN_for_Medical_Physics/functions/main_run_functions/run_supervisory.py
+++ FlexCNN_for_Medical_Physics/functions/main_run_functions/run_supervisory.py
@@ -355,7 +355,10 @@ def run_SUP(config, paths, settings):
                     print('Last Batch SSIM: ', calculate_metric(target, CNN_output, SSIM))
                     print('Last Batch LDM: ', patchwise_moment_metric(target, CNN_output, return_per_moment=True))
                     print('Input Sinogram:')
-                    show_single_unmatched_tensor(input_[0:2], fig_size=5)
+                    if input_.shape[1] == 1:
+                        show_single_unmatched_tensor(input_[0:2], fig_size=5)
+                    else
+                        show_single_unmatched_tensor(input_[0:2], fig_size=15)
                     print(input_.shape)
                     print('Target/Output:')
                     show_multiple_matched_tensors(target[0:8], CNN_output[0:8])
